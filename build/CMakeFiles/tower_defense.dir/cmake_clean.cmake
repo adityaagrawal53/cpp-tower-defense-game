c909fd9a5ce55e481e8369da200266d8d9cfce5b
Nguyen Hoang <nguyen.hoang@aalto.fi>
2023-10-30 19:16:22 +0200

    Makefile and main.cpp test file

---
+++ build/CMakeFiles/tower_defense.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tower_defense.dir/src/main.cpp.o"
+  "CMakeFiles/tower_defense.dir/src/main.cpp.o.d"
+  "tower_defense"
+  "tower_defense.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tower_defense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
