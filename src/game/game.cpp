c486289411d1a6211d0170bde34bfb757395e5b3
Aditya Agrawal <agrawaa2@aalto.fi>
2023-11-24 18:06:25 +0200

    Added basic game class

---
+++ src/game/game.cpp
@@ -0,0 +1,145 @@
+// Game.h
+
+#include "game.hpp"
+
+// Game.cpp
+
+
+Game::Game() : window(sf::VideoMode(800, 600), "Tower Defense Game"), map(50, 10, 10, "background_image.jpg", "map.txt") {
+    // Initialize game variables
+    playerHealth = 100;
+    playerScore = 0;
+    currentWave = 1;
+
+    // Initialize game elements
+    initialize();
+}
+
+void Game::initialize() {
+    // Load the first round
+    loadRound(currentWave);
+}
+
+void Game::run() {
+    while (window.isOpen()) {
+        handleInput();
+        update();
+        render();
+    }
+}
+
+void Game::handleInput() {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            window.close();
+        }
+
+        // Handle mouse input for map
+        map.handleMouseInput(event);
+    }
+}
+
+void Game::update() {
+    // Update game logic, enemy movement, tower attacks, etc.
+
+    // Example: Update enemy positions
+    for (auto& enemy : enemies) {
+        enemy.move();
+
+        // Check for collisions with towers
+        for (auto& tower : towers) {
+            if (tower.inRange(enemy)) {
+                // Example: If the tower is in range, decrease enemy health
+                enemy.takeDamage(tower.getDamage());
+            }
+        }
+
+        // Check for collisions with the end of the path
+        if (map.isEnemyAtEnd(enemy)) {
+            // Example: If enemy reaches the end, decrease player health
+            playerHealth -= enemy.getAttack();
+        }
+    }
+
+    // Check if the round is completed
+    if (enemies.empty()) {
+        currentWave++;
+        loadRound(currentWave);
+    }
+
+    // Check if the player has lost
+    if (playerHealth <= 0) {
+        // Handle game over (display message, reset game, etc.)
+        // For now, let's just close the window
+        window.close();
+    }
+}
+
+void Game::render() {
+    window.clear();
+
+    // Render game elements
+    map.draw(window);
+
+    // Render enemies
+    for (const auto& enemy : enemies) {
+        // Example: Render enemy sprite at enemy.getXPos(), enemy.getYPos()
+    }
+
+    // Render towers
+    for (const auto& tower : towers) {
+        // Example: Render tower sprite at tower.getXPos(), tower.getYPos()
+    }
+
+    // Render UI elements (score, health, etc.)
+    // Example: Render player score and health at the top of the window
+
+    window.display();
+}
+
+void Game::loadRound(int roundNumber) {
+    // Load enemies and setup for the given round
+    spawnEnemiesForRound(roundNumber);
+}
+
+void Game::spawnEnemiesForRound(int roundNumber) {
+    // Clear existing enemies and towers
+    enemies.clear();
+    towers.clear();
+
+    // Example: Load enemies and towers for each round
+    // Adjust the number, type, and difficulty of enemies as needed
+    switch (roundNumber) {
+        case 1:
+            // Load enemies for round 1
+            enemies.push_back(Enemy(/* parameters for enemy 1 */));
+            break;
+
+        case 2:
+            // Load enemies for round 2
+            enemies.push_back(Enemy(/* parameters for enemy 1 */));
+            enemies.push_back(Enemy(/* parameters for enemy 2 */));
+            break;
+
+        // Add more cases for additional rounds
+
+        default:
+            // Handle completion of all rounds (victory)
+            // For now, let's close the window
+            window.close();
+            break;
+    }
+}
+
+
+ame::Game() : window(sf::VideoMode(800, 600), "Tower Defense Game"), clock(), playerHealth(100), playerScore(0), currentWave(1) {
+    // Initialize game elements
+    initialize();
+}
+
+void Game::initialize() {
+    // Load the first wave
+    loadWave(currentWave);
+}
+
---
+++ src/game/game.hpp
@@ -0,0 +1,43 @@
+// Game.h
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "../enemy/enemy.hpp"
+#include "../tower/tower.hpp"
+#include "../map/map.hpp"
+
+#include <fstream>
+
+class Game {
+public:
+    Game();
+
+    // Game initialization
+    void initialize();
+
+    // Main game loop
+    void run();
+
+private:
+    sf::RenderWindow window;
+    GridMap map;
+    std::vector<Enemy> enemies;
+    std::vector<Tower> towers;
+    sf::Clock clock;
+
+    // Game variables
+    int playerHealth;
+    int playerScore;
+    int currentWave;
+    int playerMoney;
+
+    // Methods to handle game logic
+    void handleInput();
+    void update();
+    void render();
+
+    // Helper methods
+    void loadWave(int waveNumber);
+    void loadMap(const std::string& mapConfigFile);
+    void loadEnemies(const std::string& enemyConfigFile);
+};
