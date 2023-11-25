f1fab16fe61df0a720878e336a698387c7a93c14
Nguyen Hoang <nguyen.hoang@aalto.fi>
2023-11-25 16:30:12 +0200

    enemy-tree die updated

--- src/enemy/enemy-tree.cpp
+++ src/enemy/enemy-tree.cpp
@@ -3,11 +3,9 @@
 TreeEnemy::TreeEnemy(Game* game, std::queue<Checkpoint> checkpoints): Enemy(game, 100, 0.3, 5, 30, checkpoints) { }
 
 void TreeEnemy::die() {
-    
-    game_->getEnemies().push_back(PlantEnemy(game_, getCheckpoints()))
-    //;
-    //PlantEnemy(getCheckpoints());
-    //PlantEnemy(getCheckpoints());
+    game_->getEnemies().push_back(PlantEnemy(game_, getCheckpoints()));
+    game_->getEnemies().push_back(PlantEnemy(game_, getCheckpoints()));
+    game_->getEnemies().push_back(PlantEnemy(game_, getCheckpoints()));
     
     dead = true;
 }
