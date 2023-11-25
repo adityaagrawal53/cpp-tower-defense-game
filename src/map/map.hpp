8bb97d791a6fb326f33f8abf071b5207fe2f7ecd
Nguyen Dung <dung.5.nguyen@aalto.fi>
2023-11-25 12:01:12 +0200

    Update map.hpp

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -1,10 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <fstream>
+#include <string>
 
 class GridMap {
 public:
-    GridMap(int gridSize, int rows, int cols, const std::string& backgroundImageFile, const std::string& mapFile);
+    GridMap(int gridSize, int rows, int cols, const std::vector<std::string>& backgroundImageFiles, const std::string& mapFile, const std::vector<std::string>& towerImageFiles);
 
     void draw(sf::RenderWindow& window);
     void handleMouseInput(sf::Event& event);
@@ -14,9 +15,12 @@ private:
     int rows;
     int cols;
     std::vector<std::vector<bool>> grid;
-    sf::Texture backgroundImageTexture;
+    std::vector<sf::Texture> backgroundTextures;
     sf::Sprite backgroundImageSprite;
+    std::vector<sf::Texture> towerTextures;
+    sf::Sprite towerSprite;
 
     void loadMapFromFile(const std::string& mapFile);
+    void updateGridImage(int x, int y);
+    void updateBackgroundImage();
 };
-
