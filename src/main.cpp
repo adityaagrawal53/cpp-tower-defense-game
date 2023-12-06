9e2293f8d57869ce8c46b4ca4edac0bf2815e9de
Aditya Agrawal <agrawaa2@aalto.fi>
2023-12-06 20:20:52 +0200

    bug squashing

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 
 >>>>>>> e9accde5519df53723d3213e82df8f389cf137a6
 #include "menu/menu.hpp"
+#include "map/map.hpp"
+#include "game/game.hpp"
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Main Menu");
@@ -34,6 +36,16 @@ int main() {
             // Add code to start the game or transition to the next screen
             // For testing purposes, let's print a message
             std::cout << "Start button pressed!\n";
+            std::vector<std::string> backgroundFiles = {"map/default/background1.jpg", "map/default/background2.jpg", "map/default/background3.jpg"};
+            int selectedBackgroundIndex = 1; // Change this index to select a different background
+
+            const int gridSize = 32;
+            const int windowSize = gridSize * 16;
+
+            GridMap grid(gridSize, windowSize, "map/default/map.txt", backgroundFiles, selectedBackgroundIndex);
+
+            grid.loadMap("map/default/map.txt");
+            Game game(map);
         } else if (result == 2) {
             // Quit button pressed
             window.close();
