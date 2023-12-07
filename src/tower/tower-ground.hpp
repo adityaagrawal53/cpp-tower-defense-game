202986083d610986c6572087518439147033bcdb
Xiong Xiong <xiong.xiong@aalto.fi>
2023-12-07 13:15:08 +0200

    Update 2 files
    - /src/tower/tower-boss.hpp
    - /src/tower/tower-ground.hpp

---
+++ src/tower/tower-ground.hpp
@@ -0,0 +1,29 @@
+#ifndef TOWER_GROUND_HPP
+#define TOWER_GROUND_HPP
+
+#include <string>
+#include "tower.hpp"
+
+class Ground : public Tower {
+public:
+    Ground(std::string name = "Ground", int damage = 0, int hp = 500, double range = 0.0, int cost = 15, int damageOverTime = 0);
+
+    void draw(sf::RenderWindow& window) {
+        sf::Texture texture;
+
+        if(!texture.loadFromFile("")) {
+            std::cout << "Could not load GroundTower sprite" << std::endl;
+            return;
+        }
+
+        sf::Sprite towerSprite;
+        towerSprite.setTexture(texture);
+        towerSprite.setOrigin(towerSprite.getGlobalBounds().width, towerSprite.getGlobalBounds().height);
+        towerSprite.setPosition(getPosition().x, getPosition().y);
+        window.draw(towerSprite);
+    }
+
+private:
+};
+
+#endif
