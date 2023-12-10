1119dda4fcae7ce6e4af5d7310a7cfdfb2de7f01
nguyend27 <dung.5.nguyen@aalto.fi>
2023-12-10 15:42:47 +0200

    side menu button added

--- src/map/sidemenu.cpp
+++ src/map/sidemenu.cpp
@@ -3,7 +3,7 @@
 SideMenu::SideMenu(int windowWidth, int windowHeight, int menuWidth) {
 
     menuBackground.setSize(sf::Vector2f(menuWidth, windowHeight));
-    menuBackground.setFillColor(sf::Color(200, 200, 200));
+    menuBackground.setFillColor(sf::Color(161, 102, 47));
     menuBackground.setPosition(windowWidth - menuWidth, 0);
 
     font.loadFromFile("arial.ttf"); 
@@ -11,17 +11,28 @@ SideMenu::SideMenu(int windowWidth, int windowHeight, int menuWidth) {
     towerText.setCharacterSize(20);
     towerText.setFillColor(sf::Color::Black);
 
-    button.setTexture(buttonTexture);
+    // Load button texture
+    if (!buttonTexture.loadFromFile("button_texture.png")) {
+        // Handle texture loading error
+        std::cout <<"Error button texture loading";
+    }
 
+    button.setTexture(buttonTexture);
 }
 
-void SideMenu::handleMouseInput(sf::Event::MouseButtonEvent& mouseEvent) {
+
+int SideMenu::handleMouseInput(sf::Event::MouseButtonEvent& mouseEvent) {
     // Handle mouse input for tower selection
     if (mouseEvent.button == sf::Mouse::Left) {
         // Check if the mouse click is within the side menu
         if (mouseEvent.x > menuBackground.getPosition().x && mouseEvent.y < menuBackground.getSize().y) {
-            // Calculate the tower index based on mouse position or any other method
-        
+            // Check if the mouse click is within the button
+            sf::FloatRect buttonBounds = button.getGlobalBounds();
+            if (buttonBounds.contains(mouseEvent.x, mouseEvent.y)) {
+                // Mouse click occurred within the button
+                std::cout << "Button Clicked!" << std::endl;
+                return 1;
+            } 
         }
     }
 }
@@ -33,6 +44,10 @@ void SideMenu::draw(sf::RenderWindow& window) {
     // Adjust position and layout based on your design
     towerText.setPosition(menuBackground.getPosition().x + 10, menuBackground.getPosition().y + 10); // Adjusted position
     window.draw(towerText);
+
+    // Draw button at a specific position
+    button.setPosition(menuBackground.getPosition().x + 10, menuBackground.getPosition().y + 50);
+    window.draw(button);
 }
 
 //set the side menu to the right of the map
--- src/map/sidemenu.hpp
+++ src/map/sidemenu.hpp
@@ -2,14 +2,15 @@
 #define SIDEMENU_HPP
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 class SideMenu {
 public:
     SideMenu(int windowWidth, int windowHeight, int menuWidth);
-    void handleMouseInput(sf::Event::MouseButtonEvent& mouseEvent);
+    int handleMouseInput(sf::Event::MouseButtonEvent& mouseEvent);
     void draw(sf::RenderWindow& window);
     void setPosition(sf::Vector2f position);
-    int getSelectedTower() const;
+
 
 private:
     sf::RectangleShape menuBackground;
